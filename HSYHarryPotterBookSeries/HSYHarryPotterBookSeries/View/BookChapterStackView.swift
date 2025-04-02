5858bdfef0b8f4996417f7a19dadf492cff93f85
seoyounghan <happyunbd364@naver.com>
2025-04-02 16:11:32 +0900

    feat: [#3] level4 스크롤뷰 & 챕터 기능 추가

---
+++ HSYHarryPotterBookSeries/HSYHarryPotterBookSeries/View/BookChapterStackView.swift
@@ -0,0 +1,47 @@
+//
+//  BookChapterStackView.swift
+//  HSYHarryPotterBookSeries
+//
+//  Created by hanseoyoung on 4/2/25.
+//
+
+import UIKit
+
+final class BookChapterStackView: UIStackView {
+    let tittleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Chapters"
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupViews()
+    }
+
+    func setupViews() {
+        axis = .vertical
+        distribution = .fillEqually
+        spacing = 8
+
+        addArrangedSubview(tittleLabel)
+    }
+
+    // MARK: - 외부에서 데이터를 업데이트하는 메서드
+    /// chapters: 챕터 배열
+    func updateContent(chapters: [String]) {
+        for chapter in chapters {
+            let chapterLabel = UILabel()
+            chapterLabel.font = .systemFont(ofSize: 14, weight: .bold)
+            chapterLabel.textColor = .systemGray
+            chapterLabel.text = chapter
+            addArrangedSubview(chapterLabel)
+        }
+    }
+}
